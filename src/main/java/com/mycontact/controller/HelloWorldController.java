22c8e28547ad8e5b1f72fe69457773e837264fdf
jwlee-9939-study <jwlee9939@gmail.com>
2023-03-11 22:31:48 +0900

    first commit

---
+++ src/main/java/com/mycontact/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.mycontact.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/helloWorld")
+    public String helloWorld() {
+        return "HelloWorld";
+    }
+
+}
