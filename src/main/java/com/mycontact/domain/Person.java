2aa8c848df1948c9faf69c5b948934e4ea8ae57c
jwlee-9939-study <jwlee9939@gmail.com>
2023-03-17 22:53:54 +0900

    Lombok (2) - 1

--- src/main/java/com/mycontact/domain/Person.java
+++ src/main/java/com/mycontact/domain/Person.java
@@ -3,21 +3,26 @@ package com.mycontact.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Person {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
 
     private String hobby;
@@ -26,11 +31,24 @@ public class Person {
 
     private String address;
 
-    private LocalDateTime birthDay;
+    private LocalDate birthDay;
 
     private String job;
 
     @ToString.Exclude
     private String phoneNumber;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(id, person.id) && name.equals(person.name) && Objects.equals(hobby, person.hobby) && Objects.equals(bloodType, person.bloodType) && Objects.equals(address, person.address) && Objects.equals(birthDay, person.birthDay) && Objects.equals(job, person.job) && Objects.equals(phoneNumber, person.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, hobby, bloodType, address, birthDay, job, phoneNumber);
+    }
 
 }
